b990b3289b1c12d7329a419dba6d52aa2dd90246
Temmarie <temmar_whyte16@yahoo.com>
2020-07-10 10:23:32 +0100

    events association and foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_10_084952) do
+ActiveRecord::Schema.define(version: 2020_07_10_092012) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "location"
+    t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
