2e2f08fbade8319d3841adf8124900b9148d8069
Temmarie <temmar_whyte16@yahoo.com>
2020-07-10 11:14:04 +0100

    adding events features

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,51 @@
 class EventsController < ApplicationController
   def index
   end
+
+  def new
+		@event = current_user.events.build
+	end
+
+	def create
+		@event = current_user.events.build(event_params)
+		if @event.save
+			flash[:success] = "Your event was created successfully."
+			redirect_to event_path(@event)
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		# Uses find_event method
+	end
+
+	def edit
+		# Uses find_event method
+	end
+
+	def update
+		if @event.update(event_params)
+			flash[:success] = "Your event was updated successfully"
+			redirect_to @event
+		else
+			render 'new'
+		end
+	end
+
+	def destroy
+		@event.destroy
+		flash[:danger] = "Your event was DELETED successfully!"
+		redirect_to root_path
+	end
+
+	private
+
+		def find_event
+			@event = Event.find(params[:id])
+		end
+
+		def event_params
+			params.require(:event).permit(:title, :description, :location, :date )
+		end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_10_092012) do
+ActiveRecord::Schema.define(version: 2020_07_10_092550) do
 
   create_table "events", force: :cascade do |t|
     t.string "location"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_07_10_092012) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "creator_id"
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
